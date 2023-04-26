707817a352e0c03a8de34401918af9392a284566
임서영 <sylim0318@naver.com>
2023-04-26 10:59:08 +0900

    add

---
+++ src/test/java/data/collection/service/dcs/DcsApplicationTests.java
@@ -0,0 +1,13 @@
+package data.collection.service.dcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
