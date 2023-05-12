71cd40cfd7efac5b37f3dec12b000135c3d26d8d
임서영 <sylim0318@naver.com>
2023-05-12 10:26:47 +0900

    파일구조 변경

--- src/main/java/fassto/fulfillment/platfrom/FfpApplication.java
+++ src/main/java/fassto/fulfillment/platfrom/FfpApplication.java
@@ -1,4 +1,4 @@
-package fassto.fulfillment.ffp;
+package fassto.fulfillment.platfrom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
